fde612492773ef70fc23ba41a3060037e6b39367
Dan Cooper <cooperda.5@gmail.com>
2019-05-23 11:51:04 +0700

    removes mobile header to tst a deployment bug

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,19 @@
 //Modules
 import React from "react"
-import Media from "react-media"
+// import Media from "react-media"
 //Sass
 import styles from "./Layout.module.scss"
 import "./base.scss"
 //Components
 import Header from "./Header/Header"
-import HeaderMobile from "./HeaderMobile/HeaderMobile"
+// import HeaderMobile from "./HeaderMobile/HeaderMobile"
 
 const Layout = ({ children }) => (
   <main className={styles.Layout}>
-    <Media query="(max-width: 768px)">
+    {/* <Media query="(max-width: 768px)">
       {matches => (matches ? <HeaderMobile /> : <Header />)}
-    </Media>
+    </Media> */}
+    <Header />
     <div className={styles.Container}>{children}</div>
   </main>
 )
