83829cfe1e1af749a0d718d697b9ed3002aafa18
Dan Cooper <cooperda.5@gmail.com>
2019-05-24 10:19:55 +0700

    combines navigation components

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,39 +1,66 @@
 //Modules
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 //Sass
 import styles from "./Header.module.scss"
 //Images
 import logo from "../../../images/logo-old.png"
+//Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
-const Header = () => (
-  <header className={styles.Header}>
-    <div className={styles.NavLinksLeft}>
-      <Link activeStyle={{ fontWeight: "bold" }} to="/">
-        Home
-      </Link>
+const Header = () => {
+  const [menu, toggleMenu] = useState(false)
 
-      <Link activeStyle={{ fontWeight: "bold" }} to="/programs">
-        Our Programs
-      </Link>
-    </div>
+  const navLinks = menu
+    ? `${styles.NavLinks}`
+    : `${styles.NavLinks} ${styles.Hidden}`
 
-    <div className={styles.MainLogo}>
-      <Link to="/">
-        <img src={logo} alt="Sang Foundation Logo" />
-      </Link>
-    </div>
+  return (
+    <header className={styles.Header}>
+      <div className={styles.NavLinksLeft}>
+        <Link activeStyle={{ fontWeight: "bold" }} to="/">
+          Home
+        </Link>
 
-    <div className={styles.NavLinksRight}>
-      <Link activeStyle={{ fontWeight: "bold" }} to="/about">
-        About Us
-      </Link>
+        <Link activeStyle={{ fontWeight: "bold" }} to="/programs">
+          Our Programs
+        </Link>
+      </div>
 
-      <Link to="/contact" activeStyle={{ fontWeight: "bold" }}>
-        Contact
-      </Link>
-    </div>
-  </header>
-)
+      <div className={styles.MainLogo}>
+        <Link to="/">
+          <img src={logo} alt="Sang Foundation Logo" />
+        </Link>
+      </div>
+
+      <div className={styles.NavLinksRight}>
+        <Link activeStyle={{ fontWeight: "bold" }} to="/about">
+          About Us
+        </Link>
+
+        <Link to="/contact" activeStyle={{ fontWeight: "bold" }}>
+          Contact
+        </Link>
+      </div>
+
+      <button
+        className={styles.HamburgerMenu}
+        onClick={() => {
+          toggleMenu(!menu)
+        }}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+
+      <div className={navLinks}>
+        <Link to="/">Home</Link>
+        <Link to="/programs">Our Programs</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </header>
+  )
+}
 
 export default Header
