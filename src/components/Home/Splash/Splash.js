ab7f3dac919b0ba342266416ddaf393920bf5229
Dan Cooper <cooperda.5@gmail.com>
2019-05-31 14:14:07 +0700

    tries setTimeout to fix hydration error

--- src/components/Home/Splash/Splash.js
+++ src/components/Home/Splash/Splash.js
@@ -1,15 +1,22 @@
 //Modules
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Fade from "react-reveal/Fade"
 //Sass
 import styles from "./Splash.module.scss"
 
 const Splash = () => {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setReady(true)
+    }, 2000)
+  }, [])
+
   return (
     <section className={styles.Splash}>
       <div className={styles.SplashContent}>
-        {/* Remove from static pre-render to fix animation issue */}
-        {typeof window === "undefined" ? (
+        {ready && (
           <div className={styles.SplashLeft}>
             <Fade delay={500}>
               <h1>Welcome to the Sang Foundation</h1>
@@ -18,17 +25,19 @@ const Splash = () => {
               <h2>Join the Fight Against Single-Use Plastics</h2>
             </Fade>
           </div>
-        ) : null}
+        )}
 
-        <Fade delay={2500}>
-          <div className={styles.SplashRight}>
-            <h1>Help Us Keep Thailand's Oceans Pristine</h1>
-            <a href="https://forms.gle/RwXXjtwhkYSy4tWg8">
-              {" "}
-              <button>Volunteer</button>
-            </a>
-          </div>
-        </Fade>
+        {ready && (
+          <Fade delay={2500}>
+            <div className={styles.SplashRight}>
+              <h1>Help Us Keep Thailand's Oceans Pristine</h1>
+              <a href="https://forms.gle/RwXXjtwhkYSy4tWg8">
+                {" "}
+                <button>Volunteer</button>
+              </a>
+            </div>
+          </Fade>
+        )}
       </div>
       <div className={styles.MaskedImage} />
     </section>
